fac5269d3796a2fdb8972ae16e743fd7f03d1175
nico37alonso <60229354+nico37alonso@users.noreply.github.com>
2020-04-20 11:29:22 -0400

    Add files via upload

---
+++ 4-20-2020_in_class.R
@@ -0,0 +1,41 @@
+data( economics, package = "ggplot2")
+economics$index <- 1:nrow(economics)
+economics <- economics[1:80,]
+loessMod10 <- loess(uempmed ~index, data = economics, span = 0.10)
+loessMod25 <- loess(uempmed ~index, data = economics, span = 0.25)
+loessMod50 <- loess(uempmed ~index, data = economics, span = 0.50)
+smoothed10 <- predict(loessMod10)
+smoothed25 <- predict(loessMod25)
+smoothed50 <- predict(loessMod50)
+plot( economics$uempmed, x = economics$date, type = "l", main = "Loess Smoothing and Prediction",
+      xlab = "Date", ylab = "Unemployment(Median)")
+lines(smoothed10, x = economics$date, col = "red")
+lines(smoothed25, x = economics$date, col = "green")
+lines(smoothed50, x = economics$date, col = "blue")
+
+
+####################part 2#############################
+data("cars")
+str(cars)
+plot(speed ~dist, data = cars)
+lowess( cars$speed~ cars$dist)
+lines(lowess(cars$speed ~cars$dist, f = 2/3), col = "blue")
+lines(lowess(cars$speed ~cars$dist, f = 0.8), col = "red" )
+lines(lowess(cars$speed ~cars$dist, f = 0.9), col = "green")
+lines(lowess(cars$speed ~cars$dist, f = 0.1), col = 5)
+lines(lowess(cars$speed ~cars$dist, f = 0.01), col = 6)
+
+library(MASS)
+names(iris)
+dim(iris)
+head(iris)
+set.seed(555)
+Train <- sample(1:nrow(iris), nrow(iris)/2)
+iris_Train <- iris[Train,]
+iris_Test <- iris[-Train,]
+fit1 <- lda(Species ~Sepal.Length +Sepal.Width+ Petal.Width, data = iris_Train)
+predict1 <- predict(fit1, iris_Train)
+predict1_class <- predict1$class
+table1 <- table( predict1_class, iris_Train$Species)
+table1
+sum(diag(table1))/sum(table1)
